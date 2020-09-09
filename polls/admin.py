7a872d2e95a0f2c5d80c0cabc31a95b0cbfc2d39
Dibin V M <dibinvm1@gmail.com>
2020-09-09 09:20:48 -0500

    added small bootstrap and some admin page changes

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
 
-from .models import Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_per_page = 20
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+admin.AdminSite.site_title = "Polls"
+admin.AdminSite.site_header = "Polls Administration"
+admin.site.register(Question, QuestionAdmin)
